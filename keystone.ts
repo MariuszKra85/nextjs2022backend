74adc75b0f02210206b42182c8e9d3516d52b61f
Mariusz Krawczyk <przyslony@gmail.com>
2022-10-19 18:50:19 +0100

    first commit

---
+++ keystone.ts
@@ -0,0 +1,65 @@
+import { createAuth } from '@keystone-next/auth';
+import { config, createSchema } from '@keystone-next/keystone/schema';
+import {
+  withItemData,
+  statelessSessions,
+} from '@keystone-next/keystone/session';
+import { User } from './schemas/User';
+import 'dotenv/config';
+import { sendPasswordResetEmail } from './lib/mail';
+
+const databaseURL = process.env.DATABASE_URL;
+
+const sessionConfig = {
+  maxAge: 60 * 60 * 24 * 360,
+  secret: process.env.COOKIE_SECRET,
+};
+
+const { withAuth } = createAuth({
+  listKey: 'User',
+  identityField: 'email',
+  secretField: 'password',
+  initFirstItem: {
+    fields: ['name', 'email', 'password'],
+    // TODO: Add in inital roles here
+  },
+  passwordResetLink: {
+    async sendToken(args) {
+      // send the email
+      await sendPasswordResetEmail(args.token, args.identity);
+    },
+  },
+});
+
+export default withAuth(
+  config({
+    // @ts-ignore
+    server: {
+      cors: {
+        origin: [process.env.FRONTEND_URL],
+        credentials: true,
+      },
+    },
+    db: {
+      adapter: 'mongoose',
+      url: databaseURL,
+      async onConnect(keystone) {
+        console.log('Connected to the database!');
+      },
+    },
+    lists: createSchema({
+      // Schema items go in here
+      User,
+    }),
+    ui: {
+      // Show the UI only for poeple who pass this test
+      isAccessAllowed: ({ session }) =>
+        // console.log(session);
+        !!session?.data,
+    },
+    session: withItemData(statelessSessions(sessionConfig), {
+      // GraphQL Query
+      User: 'id name email role',
+    }),
+  })
+);
